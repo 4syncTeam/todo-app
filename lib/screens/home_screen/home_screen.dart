41bb1e312cfca274998c7dcf0a1b063a38fa6143
Ali Farhadi <ali.farhadi79@gmail.com>
2020-08-23 22:16:51 +0430

    Home Screen App bar (not complete) Names ListView and seprate widgets

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/home_screen/widgets/list_container.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -11,17 +12,77 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          leading: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.0),
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100.0),
+        child: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+                child: CircleAvatar(
+                  child: Image(
+                    image: AssetImage("assets/images/Ali.jpg"),
+                  ),
+                ),
+              ),
+              FlatButton(
+                onPressed: () => {},
+                child: Icon(
+                  Icons.insert_chart,
+                  size: 35.0,
+                ),
+              ),
+            ],
+          ),
         ),
-        child: Image(
-          image: AssetImage("assets/images/Ali.jpg"),
-        ),
-      )),
+      ),
       body: Column(
-        children: <Widget>[],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Hey, Ali",
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  "Main focus for today?",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 35.0),
+          Container(
+            height: 320,
+            color: Colors.blue,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) {
+                return ListContainer();
+              },
+              itemCount: 5,
+            ),
+          ),
+        ],
       ),
     );
   }
