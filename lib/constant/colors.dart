0b1ff05ecff3ce4c53bcfb48d370e6617d69cc06
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-09-03 00:49:11 +0430

    code clean up for starting new UI

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -1,9 +1,1 @@
 import 'package:flutter/material.dart';
-
-const kPolishedPine = Color(0xFF589D80);
-const kMSUGreen = Color(0xFF26413C);
-const kWhite = Color(0xFFFCFCFC);
-const kDarkJungleGreen = Color(0xFF1e2019);
-const kGreenBlueCrayola = Color(0xFF4C8CBD);
-const kMidnightBlue = Color(0xFF031A6B);
-const kAmaranthRed = Color(0xFFD72638);
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,38 +1,12 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_app/screens/home_screen/body.dart';
 
-import 'package:todo_app/widgets/appBar.dart';
+import 'body.dart';
 
-import 'custom_bottom_app_bar.dart';
-
-class HomeScreen extends StatefulWidget {
-  HomeScreen({Key key}) : super(key: key);
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // print(FirebaseFirestore.instance.collection('todos').snapshots().first);
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100.0),
-        child: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          title: CustomAppBarTitle(),
-        ),
-      ),
       body: HomeBody(),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.transparent,
-        elevation: 0,
-        child: CustomBottomAppBar(),
-      ),
     );
   }
 }
