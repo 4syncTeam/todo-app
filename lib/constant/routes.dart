200b9503714e8864d58a980eb88d0ef85d96911c
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-09-04 20:49:43 +0430

    made route more consistans

--- lib/constant/routes.dart
+++ lib/constant/routes.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/screens/add_task_screen/add_task.dart';
+import 'package:todoapp/screens/auth_screen/auth_screen.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
-  '/add': (context) => AddTask(),
+  ADD_TASK_ROUTE: (context) => AddTask(),
+  AUTH_ROUTE: (context) => AuthScreen()
 };
+
+const String AUTH_ROUTE = '/auth';
+const String ADD_TASK_ROUTE = '/add';
