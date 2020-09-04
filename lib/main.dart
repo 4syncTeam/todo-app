f8b49c5eaa33f1815db2a244e92e3945c37c09e4
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-09-04 20:22:12 +0430

    changed folder name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import './screens/add_task_screen.dart/add_task.dart';
+import './screens/add_task_screen/add_task.dart';
 import './screens/home_screen/home_screen.dart';
 
 void main() async {
