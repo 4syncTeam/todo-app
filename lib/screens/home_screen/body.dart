dcc1c043144f05c45b8626e8652c22ff3fc81794
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-09-04 20:54:31 +0430

    initialized Auth screen

--- lib/screens/home_screen/body.dart
+++ lib/screens/home_screen/body.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/constant/routes.dart';
 
 class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: FlatButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/add');
-          },
-          child: Text('Add task'),
-        ),
+      alignment: Alignment.center,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          buildFlatButton(context, 'Add task', ADD_TASK_ROUTE),
+          buildFlatButton(context, 'Auth', AUTH_ROUTE),
+        ],
       ),
+    );
+  }
+
+  FlatButton buildFlatButton(BuildContext context, String text, String route) {
+    return FlatButton(
+      onPressed: () {
+        Navigator.pushNamed(context, route);
+      },
+      child: Text(text),
     );
   }
 }
