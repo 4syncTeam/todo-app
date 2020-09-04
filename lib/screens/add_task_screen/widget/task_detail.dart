95e7cef928486c0e43771ecbebbe18654aed247a
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-09-04 20:32:21 +0430

    code cleanup

--- lib/screens/add_task_screen/widget/task_detail.dart
+++ lib/screens/add_task_screen/widget/task_detail.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:intl/intl.dart';
 
 import '../../../constant/colors.dart';
@@ -30,7 +29,7 @@ class _TaskDetailState extends State<TaskDetail> {
           ),
         ],
       ),
-      child: Padding(
+      child: Container(
         padding: const EdgeInsets.all(25.0),
         child: Column(
           children: <Widget>[
@@ -41,84 +40,21 @@ class _TaskDetailState extends State<TaskDetail> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Date'),
-                    RaisedButton(
-                      onPressed: () {
-                        showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(2019),
-                          lastDate: DateTime(2022),
-                        ).then(
-                          (value) {
-                            setState(() {
-                              _selectedDate = value;
-                            });
-                          },
-                        );
-                      },
-                      child: _selectedDate == null
-                          ? Text('Pick a Date')
-                          : Text(_dateFormat.format(_selectedDate)),
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Time'),
-                    RaisedButton(
-                      onPressed: () {
-                        showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                        ).then(
-                          (value) {
-                            setState(() {
-                              _selectedTime = value;
-                            });
-                          },
-                        );
-                      },
-                      child: _selectedTime == null
-                          ? Text('Set a Time')
-                          : Text(_selectedTime.hour.toString() +
-                              " : " +
-                              _selectedTime.minute.toString()),
-                    ),
-                  ],
-                ),
+                pickDateSection(context),
+                pickTimeSection(context),
               ],
             ),
             SizedBox(height: 20),
             entrySection('Tag', 'Select a tag for your task'),
             SizedBox(height: 25),
-            Container(
-              width: 180,
-              child: RaisedButton(
-                color: kPrimaryColor,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0)),
-                onPressed: () => {},
-                child: Text(
-                  'Add task',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
-            ),
+            addTaskBtn(),
           ],
         ),
       ),
     );
   }
 
-  Column entrySection(String text, String hintText) {
+  Widget entrySection(String text, String hintText) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -134,6 +70,78 @@ class _TaskDetailState extends State<TaskDetail> {
           ),
         ),
       ],
+    );
+  }
+
+  Widget pickDateSection(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('Date'),
+        RaisedButton(
+          onPressed: () {
+            showDatePicker(
+              context: context,
+              initialDate: DateTime.now(),
+              firstDate: DateTime(2019),
+              lastDate: DateTime(2022),
+            ).then(
+              (value) {
+                setState(() {
+                  _selectedDate = value;
+                });
+              },
+            );
+          },
+          child: _selectedDate == null
+              ? Text('Pick a Date')
+              : Text(_dateFormat.format(_selectedDate)),
+        ),
+      ],
+    );
+  }
+
+  Widget pickTimeSection(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('Time'),
+        RaisedButton(
+          onPressed: () {
+            showTimePicker(
+              context: context,
+              initialTime: TimeOfDay.now(),
+            ).then(
+              (value) {
+                setState(() {
+                  _selectedTime = value;
+                });
+              },
+            );
+          },
+          child: _selectedTime == null
+              ? Text('Set a Time')
+              : Text(_selectedTime.hour.toString() +
+                  " : " +
+                  _selectedTime.minute.toString()),
+        ),
+      ],
+    );
+  }
+
+  Widget addTaskBtn() {
+    return Container(
+      width: 180,
+      child: RaisedButton(
+        color: kPrimaryColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+        onPressed: () => {},
+        child: Text(
+          'Add task',
+          style: TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600),
+        ),
+      ),
     );
   }
 }
