9e46ab9080c67164cc2f2a95f93b4d6c90633ba0
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-09-04 20:35:02 +0430

    code cleanup

--- lib/screens/add_task_screen/add_task.dart
+++ lib/screens/add_task_screen/add_task.dart
@@ -7,21 +7,25 @@ class AddTask extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton(
-            padding: EdgeInsets.zero,
-            onPressed: () => {},
-            child: Icon(
-              Icons.close,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
+      appBar: buildAppBar(),
       body: AddTaskBody(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: kPrimaryColor,
+      elevation: 0.0,
+      actions: <Widget>[
+        FlatButton(
+          padding: EdgeInsets.zero,
+          onPressed: () => {},
+          child: Icon(
+            Icons.close,
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
