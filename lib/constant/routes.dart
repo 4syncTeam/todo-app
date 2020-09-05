1c764512ecc429a13d0874dd85fe13ee3c8e7ec9
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-09-05 23:16:33 +0430

    init todo listview

--- lib/constant/routes.dart
+++ lib/constant/routes.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/screens/add_task_screen/add_task.dart';
 import 'package:todoapp/screens/auth_screen/auth_screen.dart';
+import 'package:todoapp/screens/todos_list_screen/todos_list_screen.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
   ADD_TASK_ROUTE: (context) => AddTask(),
-  AUTH_ROUTE: (context) => AuthScreen()
+  AUTH_ROUTE: (context) => AuthScreen(),
+  TODO_LIST_ROUTE: (context) => TodoListScreen(),
 };
 
 const String AUTH_ROUTE = '/auth';
 const String ADD_TASK_ROUTE = '/add';
+const String TODO_LIST_ROUTE = '/todo-list';
