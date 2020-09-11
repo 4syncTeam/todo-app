9ff7760a23051bc7c99b8367f49176e2fac9779c
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-09-11 20:55:29 +0430

    added todo list

--- lib/screens/todos_list_screen/body.dart
+++ lib/screens/todos_list_screen/body.dart
@@ -1,10 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/constant/colors.dart';
+import 'package:todoapp/models/todo_model.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Works!!!'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          buildTitle(),
+          buildList(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildTitle() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, top: 20, bottom: 20),
+      child: Text(
+        'Month name',
+        style: TextStyle(
+          fontWeight: FontWeight.w900,
+          fontSize: 28,
+        ),
+      ),
+    );
+  }
+
+  Widget buildList() {
+    return Expanded(
+      child: ListView.separated(
+        itemCount: DUMMY_LIST.length,
+        separatorBuilder: (context, index) => Divider(
+          height: 4,
+          color: kPrimaryColor.withOpacity(0.6),
+        ),
+        itemBuilder: (context, index) {
+          return Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                buildItemIcon(),
+                buildItemDetails(index),
+                Spacer(),
+                buildItemDate(index),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildItemDate(int index) {
+    return Container(
+      padding: EdgeInsets.only(right: 30),
+      child: Text(
+        '${DUMMY_LIST[index].dateTime.month} / ${DUMMY_LIST[index].dateTime.day}',
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+  Widget buildItemDetails(int index) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          DUMMY_LIST[index].title,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          DUMMY_LIST[index].description,
+          style: TextStyle(
+            fontWeight: FontWeight.w300,
+            fontSize: 14,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildItemIcon() {
+    return Container(
+      padding: EdgeInsets.all(30),
+      child: Icon(Icons.check_circle),
     );
   }
 }
--- lib/screens/todos_list_screen/todos_list_screen.dart
+++ lib/screens/todos_list_screen/todos_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/constant/colors.dart';
 
 import './body.dart';
 
@@ -6,6 +7,34 @@ class TodoListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: kPrimaryColor,
+        title: Container(
+          height: 42,
+          decoration: BoxDecoration(
+            color: kPrimaryColorDarken,
+            borderRadius: BorderRadius.all(Radius.circular(36)),
+          ),
+          child: TextField(
+            textInputAction: TextInputAction.search,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+            ),
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              fillColor: Colors.transparent,
+              filled: true,
+              border: InputBorder.none,
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
       body: Body(),
     );
   }
